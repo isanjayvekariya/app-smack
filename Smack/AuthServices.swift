f7dab9ea16a7d220c6f8e327fc03deb6707e5518
Sanjay Vekariya <sanjayvekariya@Sanjays-MacBook-Pro.local>
2017-08-20 00:00:53 -0400

    Crate webrequest and constants, URLs

---
+++ Smack/AuthServices.swift
@@ -0,0 +1,72 @@
+//
+//  AuthServices.swift
+//  Smack
+//
+//  Created by Sanjay Vekariya on 8/19/17.
+//  Copyright © 2017 Sanjay Vekariya. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+
+    let instance = AuthService()
+    
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn: Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken: String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail: String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String, password: String, completion: CompletionHandler) {
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let header = [
+            "content-type": "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            if response.result.error == nil {
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+        
+    }
+    
+    
+    
+    
+}
