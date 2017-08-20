4291dce007e18b4e024df88c51ea33cac0839913
Sanjay Vekariya <sanjayvekariya@Sanjays-MacBook-Pro.local>
2017-08-20 02:24:36 -0400

    create/add user

--- Smack/AuthServices.swift
+++ Smack/AuthServices.swift
@@ -8,8 +8,9 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
-class AuthService: NSObject {
+class AuthService {
 
     static let instance = AuthService()
     
@@ -43,19 +44,14 @@ class AuthService: NSObject {
     }
     
     func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
-        
         let lowerCaseEmail = email.lowercased()
-        
-        let header = [
-            "content-type": "application/json; charset=utf-8"
-        ]
         
         let body: [String: Any] = [
             "email": lowerCaseEmail,
             "password": password
         ]
         
-        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseString { (response) in
             if response.result.error == nil {
                 completion(true)
             } else {
@@ -63,9 +59,67 @@ class AuthService: NSObject {
                 debugPrint(response.result.error as Any)
             }
         }
-        
     }
     
+    func loginUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+
+        Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
+            if response.result.error == nil {
+                guard let data = response.data else {return}
+                let json = JSON(data: data)
+                self.userEmail = json["email"].stringValue
+                self.authToken = json["token"].stringValue
+                
+                self.isLoggedIn = true
+                completion(true)
+            } else {
+                completion(false)
+            }
+        }
+    }
+    
+    func createUser(name: String, email: String, avatarName: String, avatarColor: String, completion: @escaping CompletionHandler) {
+        let lowerCaseEmail = email.lowercased()
+        
+        let header: [String: String] = [
+            "Authorization": "Bearer \(AuthService.instance.authToken)",
+            "content-type": "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "name": name,
+            "email": lowerCaseEmail,
+            "avatarName": avatarName,
+            "avatarColor": avatarColor,
+        ]
+        
+        Alamofire.request(URL_USER_ADD, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseJSON { (response) in
+            if response.result.error == nil {
+                guard let data = response.data else {return}
+                let json = JSON(data: data)
+                let id = json["_id"].stringValue
+                let color = json["avatarColor"].stringValue
+                let avatarName = json["avatarName"].stringValue
+                let email = json["email"].stringValue
+                let name = json["name"].stringValue
+                
+                UserDataService.instance.setUserData(id: id, color: color, avatarName: avatarName, email: email, name: name)
+                
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+
+    }
     
     
     
--- Smack/Constants.swift
+++ Smack/Constants.swift
@@ -11,8 +11,10 @@ import Foundation
 typealias CompletionHandler = (_ Success: Bool) -> ()
 
 // URL Constants
-let BASE_URL = "https://chatchattychat.herokuapp.com/v1"
+let BASE_URL = "https://chatchattychat.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
+let URL_USER_ADD = "\(BASE_URL)user/add"
 
 // Segues
 let TO_LOGIN = "toLogin"
@@ -23,3 +25,8 @@ let UNWIND = "unwindToChannel"
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggenIn"
 let USER_EMAIL = "userEmail"
+
+// Headers
+let HEADER = [
+    "content-type": "application/json; charset=utf-8"
+]
