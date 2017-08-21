f2583b0e88f2264aed1f231644a61d5ebdca314b
Sanjay Vekariya <sanjayvekariya@Sanjays-MacBook-Pro.local>
2017-08-20 20:27:25 -0400

    avatar picket collectionview image load and select

--- Smack/AvatarPickerVC.swift
+++ Smack/AvatarPickerVC.swift
@@ -14,6 +14,9 @@ class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionVi
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var segmentControl: UISegmentedControl!
     
+    // Variables
+    var avatarType = AvatarType.dark
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +26,7 @@ class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "avatarCell", for: indexPath) as? AvatarCell {
+            cell.configureCell(index: indexPath.item, type: avatarType)
             return cell
         }
         return AvatarCell()
@@ -36,8 +40,37 @@ class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionVi
         return 28
     }
     
-    @IBAction func segmentControlChanged(_ sender: Any) {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
+        var numbOfColumns: CGFloat = 3
+        if UIScreen.main.bounds.width > 320 {
+            numbOfColumns = 4
+        }
+        
+        let spaceBetweenCell: CGFloat = 10
+        let padding: CGFloat = 40
+        
+        let cellDimension = ((collectionView.bounds.width - padding) - (numbOfColumns - 1) * spaceBetweenCell) / numbOfColumns
+        
+        return CGSize(width: cellDimension, height: cellDimension)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if avatarType == .dark {
+            UserDataService.instance.setAvatarName(avatarName: "dark\(indexPath.item)")
+        } else {
+            UserDataService.instance.setAvatarName(avatarName: "light\(indexPath.item)")
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func segmentControlChanged(_ sender: Any) {
+        if segmentControl.selectedSegmentIndex == 0 {
+            avatarType = .dark
+        } else {
+            avatarType = .light
+        }
+        collectionView.reloadData()
     }
     
     @IBAction func backPressed(_ sender: Any) {
