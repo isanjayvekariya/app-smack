4bb9e3ed777afd4d9c4e6f91070efeee7e532b10
Sanjay Vekariya <sanjayvekariya@Sanjays-MacBook-Pro.local>
2017-08-21 21:42:50 -0400

    profile view

--- Smack/ChannelVC.swift
+++ Smack/ChannelVC.swift
@@ -22,7 +22,16 @@ class ChannelVC: UIViewController {
     }
     
     @IBAction func loginBtnPresed(_ sender: Any) {
-        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        
+        if AuthService.instance.isLoggedIn {
+            // Show profile page
+            let profile = ProfileVC()
+            profile.modalPresentationStyle = .custom
+            present(profile, animated: true, completion: nil)
+        
+        } else {
+            performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        }
     }
     
     func userDataDidChanged(_notif: Notification) {
