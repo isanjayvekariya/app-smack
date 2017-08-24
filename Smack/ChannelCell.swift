b8452610b9e502d4a66030aa530644bc2c93bcc9
Sanjay Vekariya <sanjayvekariya@Sanjays-MacBook-Pro.local>
2017-08-23 20:51:39 -0400

    channel tablewiew

---
+++ Smack/ChannelCell.swift
@@ -0,0 +1,35 @@
+//
+//  ChannelCell.swift
+//  Smack
+//
+//  Created by Sanjay Vekariya on 8/23/17.
+//  Copyright © 2017 Sanjay Vekariya. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        if selected {
+            self.layer.backgroundColor = UIColor(white: 1, alpha: 0.2).cgColor
+        } else {
+            self.layer.backgroundColor = UIColor.clear.cgColor
+        }
+
+    }
+
+    func configureCell(channel: Channel) {
+        let channelName = channel.channelTitle ?? ""
+        title.text = channelName
+    }
+
+}
--- Smack/ChannelVC.swift
+++ Smack/ChannelVC.swift
@@ -8,16 +8,19 @@
 
 import UIKit
 
-class ChannelVC: UIViewController {
+class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // Outlets
     @IBOutlet weak var loginBtn: UIButton!
     @IBOutlet weak var userImg: CircleImage!
+    @IBOutlet weak var tableview: UITableView!
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {}
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableview.delegate = self
+        tableview.dataSource = self
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChanged(_notif:)), name: NOTIF_USER_DATA_DID_CHANGED, object: nil)
     }
@@ -53,5 +56,22 @@ class ChannelVC: UIViewController {
             userImg.image = UIImage(named: "menuProfileIcon")
             userImg.backgroundColor = UIColor.clear
         }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableview.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as? ChannelCell {
+            cell.configureCell(channel: MessageService.instance.channels[indexPath.row])
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MessageService.instance.channels.count
     }
 }
