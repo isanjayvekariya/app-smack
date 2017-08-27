50811b0f1683597dc1be7f3c097b80a002c38ed3
Sanjay Vekariya <sanjayvekariya@Sanjays-MacBook-Pro.local>
2017-08-27 00:33:07 -0400

    date formate of msgs

--- Smack/MessageCell.swift
+++ Smack/MessageCell.swift
@@ -25,6 +25,20 @@ class MessageCell: UITableViewCell {
         userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
         userNameLbl.text = message.userName
         messageBodyLbl.text = message.message
+        
+        guard var isoDate = message.timeStamp else { return }
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = isoDate.substring(to: end)
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStampLbl.text = finalDate
+        }
     }
-
 }
